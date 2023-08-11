ca000698b32f3f7ce8fe8353fce52c8a7a6c688a
Anders B. Clausen <anbclausen@users.noreply.github.com>
2023-08-12 00:34:26 +0200

    feat: made simple Rust service

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use mist_tools_rust::envelope::Envelope;
+use mist_tools_rust::mime_types;
+use mist_tools_rust::mist;
+use mist_tools_rust::mist::reply_str_to_origin;
+use mist_tools_rust::mist_tools::MistTools;
+use std::env;
+
+fn handle_action(_payload_bytes: Vec<u8>, _envelope: Envelope) -> Result<(), &'static str> {
+    reply_str_to_origin("Hello from Rust!", mime_types::TXT)
+        .expect("Something went wrong in handle_action");
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect();
+    mist::service(args)
+        .unwrap()
+        .handle("action", handle_action)
+        .init(|| Ok(println!("Hello, I started a Rust container")))
+        .unwrap();
 }
