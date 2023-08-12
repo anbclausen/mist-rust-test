eb9a6992ad5b1a589e69f02df978537922b92955
Anders B. Clausen <anbclausen@users.noreply.github.com>
2023-08-12 17:14:38 +0200

    feat: using new and rustified version

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
-use mist_tools_rust::envelope::Envelope;
-use mist_tools_rust::mime_types;
-use mist_tools_rust::mist;
-use mist_tools_rust::mist::reply_str_to_origin;
-use mist_tools_rust::mist_tools::MistTools;
-use std::env;
+use mist_tools::{mime_types, mist_service, reply_str_to_origin, Envelope};
 
-fn handle_action(_payload_bytes: Vec<u8>, _envelope: Envelope) -> Result<(), &'static str> {
+fn main() {
+    mist_service!({actions: {"action": handle_action}})
+}
+
+fn handle_action(_payload_bytes: Vec<u8>, _envelope: Envelope) -> Result<(), String> {
     reply_str_to_origin("Hello from Rust", mime_types::TXT)
         .expect("Something went wrong in handle_action");
     Ok(())
-}
-
-fn main() {
-    let args = env::args().collect();
-    mist::service(args)
-        .unwrap()
-        .handle("action", handle_action)
-        .init(|| Ok(println!("Hello, I started some Rust code")))
-        .unwrap();
 }
